bebc2c747512ffb6092ceb390148a65877379761
Alain Tran <alaint77@gmail.com>
2019-08-09 15:55:54 +0200

    using context + hooks look cleaner

---
+++ contextapp/src/components/BookList.jsx
@@ -0,0 +1,44 @@
+import React, { useContext } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
+import { BookContext } from "../contexts/BookContext";
+
+// class BookList extends Component {
+//     static contextType = ThemeContext;
+//     render() {
+//         const { isLightTheme, light, dark } = this.context;
+//         const theme = isLightTheme ? light : dark;
+//         return (
+//             <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
+//                 <ul>
+//                     <li style={{ background: theme.ui }}>One</li>
+//                     <li style={{ background: theme.ui }}>two</li>
+//                     <li style={{ background: theme.ui }}>threee</li>
+//                 </ul>
+//             </div>
+//         );
+//     }
+// }
+
+const BookList = () => {
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const { books } = useContext(BookContext);
+  const theme = isLightTheme ? light : dark;
+  return (
+    <div
+      className="book-list"
+      style={{ color: theme.syntax, background: theme.bg }}
+    >
+      <ul>
+        {books.map(book => {
+          return (
+            <li key={book.id} style={{ background: theme.ui }}>
+              {book.title}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default BookList;
