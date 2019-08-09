505cd4259b25d6abfa656e2c4c5d78aa8e1d9c3c
Alain Tran <alaint77@gmail.com>
2019-08-09 12:42:48 +0200

    contexts done

---
+++ src/components/Booklist.jsx
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
+
+class BookList extends Component {
+    static contextType = ThemeContext;
+    render() { 
+        const { isLightTheme, light, dark } = this.context;
+        const theme = isLightTheme ? light : dark;
+        return ( 
+            <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
+                <ul>
+                    <li style={{ background: theme.ui }}>One</li>
+                    <li style={{ background: theme.ui }}>two</li>
+                    <li style={{ background: theme.ui }}>threee</li>
+                </ul>
+            </div>
+        );
+    }
+}
+ 
+export default BookList;
